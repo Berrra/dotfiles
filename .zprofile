20a88a6965e424108118c9d01a414fa749015b6d
Berrra <jonas@bergkvist.com>
2018-08-14 15:01:05 +0200

    2018-08-14

--- .zprofile
+++ .zprofile
@@ -7,13 +7,6 @@ export XDG_CONFIG_HOME
 EDITOR=/usr/bin/vim
 export EDITOR
 
-PANEL_FIFO=/tmp/panel-fifo
-PANEL_HEIGHT=24
-PANEL_FONT="Source Code Pro:size=12"
-PANEL_FONT2="FontAwesome:size=14"
-PANEL_VM_NAME=bspwm_panel
-export PANEL_FIFO PANEL_HEIGHT PANEL_FONT PANEL_FONT2 PANEL_WM_NAME
-
 # Steam fix
 export DISTRIB_ID=arch
 export DISTRIB_RELEASE=$(uname -r)
--- bin/update_dotfiles.sh
+++ bin/update_dotfiles.sh
@@ -9,6 +9,8 @@ cp -f ~/.zshrc ~/dotfiles
 cp -f ~/.zprofile ~/dotfiles
 cp -f ~/.config/bspwm/bspwmrc ~/dotfiles/.config/bspwm/bspwmrc
 cp -f ~/.config/sxhkd/sxhkdrc ~/dotfiles/.config/sxhkd/sxhkdrc
+cp -f ~/.config/polybar/config ~/dotfiles/.config/polybar/config
+cp -f ~/.config/polybar/launch.sh ~/dotfiles/.config/polybar/launch.sh
 cp -f ~/.xinitrc ~/dotfiles
 cp -f ~/.Xresources ~/dotfiles
 
